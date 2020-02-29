97d5544d5951c1e2463b70781354b26fc916f35a
atabbakh1 <54362791+atabbakh1@users.noreply.github.com>
2020-02-29 01:24:08 -0500

    Tint Slider
    Alpha color value of Glass material is controlled with a slider to manipulate transparency

---
+++ solVR/Assets/Scripts/Material_Modification_AT.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Material_Modification_AT : MonoBehaviour
+
+{
+    //grab the glass material assigned in the editor
+    public Material glassMaterial;
+    
+    //this slider will control the alpha channel values of the original color tint for the glass material 
+    public Slider tintColorSlider;
+
+    //this will store the color tint value of the glass material
+    private Color tintColor;
+
+    public Button fritOnOff;
+    public Slider fritScaleSlider;
+
+    private float fritScale;
+
+    void Start()
+    {
+        //grab the current color of the glass material
+        tintColor = glassMaterial.color;
+    }
+
+
+    void Update()
+    {
+        //grab the alpha channel of the glass material color and assign the slider value to it.
+        tintColor.a = tintColorSlider.value;
+
+        //sets the modified color value back to the glass material 
+        glassMaterial.SetColor("_BaseColor", tintColor);
+    }
+}
