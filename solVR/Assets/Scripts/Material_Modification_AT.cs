8ea4b54a9002cb12bd2d5518fe076703ca42464e
lukegehron <luke@gehron.com>
2020-02-29 10:53:31 -0500

    things changed

--- solVR/Assets/Scripts/Material_Modification_AT.cs
+++ solVR/Assets/Scripts/Material_Modification_AT.cs
@@ -26,6 +26,8 @@ public class Material_Modification_AT : MonoBehaviour
     public Slider fritScaleSlider;
     public Material fritGlassMat;
     private Vector2 fritScale;
+
+    public GameObject[] sliders;
     
 
 
@@ -45,17 +47,24 @@ public class Material_Modification_AT : MonoBehaviour
     void Update()
     {
         //grab the alpha channel of the glass material color and assign the slider value to it.
+        float currentPos = (sliders[0].transform.position.y - 1.06f) * 2.0f;
         tintColor.a = tintColorSlider.value;
+        tintColor.a = currentPos;
 
         //sets the modified color value back to the glass material 
         glassMaterial.SetColor("_BaseColor", tintColor);
 
         //convert the value to 1 to 0 with absolute value and assign back to Depth Bias
+
         shadowBias = Mathf.Abs(tintColorSlider.value-1);
+        shadowBias = Mathf.Abs(currentPos - 1);
         sunLight.shadowBias = shadowBias;
 
         //creates a new vector2 for scaling using the fritScaleSlider value
-        Vector2 newFritScale = new Vector2(fritScale.x*fritScaleSlider.value, fritScale.y*fritScaleSlider.value);
+        currentPos = (sliders[1].transform.position.y - 1.06f) * 2.0f;
+        Debug.Log("Your current pos" + currentPos);
+        //Vector2 newFritScale = new Vector2(fritScale.x*fritScaleSlider.value, fritScale.y*fritScaleSlider.value);
+        Vector2 newFritScale = new Vector2(fritScale.x * currentPos, fritScale.y * currentPos);
         //assigns the new fritscale to the main texture of the GlassFirt Material
         fritGlassMat.SetTextureScale("_BaseMap", newFritScale);        
     }
